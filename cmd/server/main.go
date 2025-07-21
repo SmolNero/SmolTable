eff47ecd75f6ae12106ef88bd564ada763972440
SMOL <smolnero@gmail.com>
2025-07-20 21:07:04 -0600

    HTT

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+// cmd/server/main.go
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting SmolTable on http://localhost:8080")
+
+	//Test route
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintln(w, Welome to SmolTable 🎉)
+	})
+
+	err := http.ListenAndServer("8080", nil)
+	if err != nil {
+		log.Fatal("Server failed to start: %v", err)
+	}
+}
