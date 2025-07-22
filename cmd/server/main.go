3354b72dfd1c52038289cf06b2c7b9e5b75594ba
SMOL <smolnero@gmail.com>
2025-07-21 20:56:04 -0600

    main.go cleanup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,14 +8,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Starting SmolTable on http://localhost:8080")
+	fmt.Println("Starting SmolTable on http://localhost:8080")	// Registers a rout** for root path
 
 	//Test route
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintln(w, Welome to SmolTable 🎉)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w," Welome to SmolTable 🎉")
 	})
 
-	err := http.ListenAndServe("8080", nil)
+	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
 		log.Fatal("Server failed to start: %v", err)
 	}
+ }/
