3b34a3f188d7978301a3d5bd6da36f1eb0c0a978
SMOL <smolnero@gmail.com>
2025-08-13 18:47:42 -0600

    olo

--- internal/ricefields/rice_test.go
+++ internal/ricefields/rice_test.go
@@ -11,4 +11,9 @@ import(
 func TestInitDB(t *testing.t) {
 	schemeDir := filepath.Join("internal", "ricefields")
 	schemaPath := filepath.Join(schemeDir, "schemea.sql")
+	if err := os.MkdirAll(schemeDir, 0o755); err != nil {
+		t.Fatalf("failed to create schema dir: %v", err)
+	}
+
+	
 }
